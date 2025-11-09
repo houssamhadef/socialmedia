6ecc68d207675ce52a4b9c3a1d1bac4d56f0874f
SlayerDevv <hdfhoussam18@gmail.com>
2025-11-09 16:12:17 +0100

    ignore eslint

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,13 @@ import { config } from 'process';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+   eslint: {
+      ignoreDuringBuilds: true,
+   }
    images: {
     domains: ['slayerdev056792.s3.amazonaws.com', 'localhost', '41.98.114.72']
    },
+   
    
 };
 
